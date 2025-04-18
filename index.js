7b655f35e665f96806ff0fc6f106f7e67afb73f4
codersjj <shanewestlife@outlook.com>
2025-04-18 22:13:49 +0800

    feat: ES6 vs Nodejs

--- index.js
+++ index.js
@@ -1,31 +1,44 @@
+// in node.js
+// require()
+
+// in front-end javascript you can't use require
+// you can use import
+
+import { ethers } from "./ethers-6.7.0.min.js"
+console.log("ethers", ethers)
 let isConnected = false
 
+const connectBtn = document.getElementById("connectBtn")
+const fundBtn = document.getElementById("fundBtn")
+connectBtn.onclick = connect
+fundBtn.onclick = fund
+
 async function connect() {
-  if (typeof window.ethereum !== 'undefined') {
+  if (typeof window.ethereum !== "undefined") {
     console.log("I see a metamask!")
     try {
       await window.ethereum.request({ method: "eth_requestAccounts" })
     } catch (error) {
       console.error("Error connecting to metamask:", error)
-      document.getElementById("connectBtn").innerHTML = "Error connecting to Metamask"
+      connectBtn.innerHTML = "Error connecting to Metamask"
       return
     }
     updateButtonText(true)
   } else {
-    console.log('No metamask!')
-    document.getElementById("connectBtn").innerHTML = "Please install Metamask"
+    console.log("No metamask!")
+    connectBtn.innerHTML = "Please install Metamask"
   }
 }
 
 async function checkConnection() {
-  if (typeof window.ethereum !== 'undefined') {
+  if (typeof window.ethereum !== "undefined") {
     let accounts = []
     try {
       accounts = await window.ethereum.request({ method: "eth_accounts" })
     } catch (error) {
       console.error("Error checking connection:", error)
-      document.getElementById("connectBtn").innerHTML = "Error checking connection"
-      return 
+      connectBtn.innerHTML = "Error checking connection"
+      return
     }
     updateButtonText(accounts.length > 0)
   }
@@ -33,12 +46,11 @@ async function checkConnection() {
 
 function updateButtonText(isConnect) {
   if (isConnect && isConnected) return
-  
+
   console.log("Updating button text...")
 
-  const button = document.getElementById('connectBtn')
-  button.innerHTML = isConnect ? "Connected" : "Connect"
-  
+  connectBtn.innerHTML = isConnect ? "Connected" : "Connect"
+
   isConnected = isConnect
 }
 
@@ -48,3 +60,16 @@ window.ethereum.on("accountsChanged", () => {
   console.log("Accounts changed")
   checkConnection()
 })
+
+// fund function
+async function fund(ethAmount) {
+  console.log(`Funding with ${ethAmount}...`)
+  if (typeof window.ethereum !== "undefined") {
+    // provider / connection to the blockchain
+    // signer / wallet / someone with some gas
+    // contract that we are interacting with
+    // ABI & Address
+  }
+}
+
+// withdraw
